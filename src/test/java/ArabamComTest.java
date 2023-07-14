ba62d3c488b68b15d823f78743e841d459ee2a60
lutfi <lutfikavalci@gmail.com>
2023-07-14 22:00:16 +0300

    LTF

---
+++ src/test/java/ArabamComTest.java
@@ -0,0 +1,37 @@
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+public class ArabamComTest {
+
+
+    // Arabam kac para bolumune tiklayalim
+// Aracimin fiyatini merak ediyorum bolumunetiklayalim
+// Wolkswagen markasini secelim
+// yil secimi yapalim
+// model secimi yapalim
+// govde tipini secelim
+// yakit tipini secelim
+// vites tipini secelim
+// Versiyon secimi yapalim
+// aracin km bilgilerini girelim
+// aracin rengini secelim
+// opsiyel donanim (varsa) seecelim
+// degisen bilgisi ekleyerek tramer kaydi belirtelim
+// aracimizin fiyatinin 500.000 tl den fazla oldugunu test edelim
+// uygulamayi kapatalim
+
+    @Test
+    public void arabamtest(){
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"PIXEL");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10.0");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
+
+        capabilities.setCapability("appPackage", "com.dogan.arabam"); // com.dogan.arabam-->Bu uygulamanın kimliği oluyor.
+        capabilities.setCapability("appActivity", "");
+    }
+}
